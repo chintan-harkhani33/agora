2758d461eccd91b0fe1593d518c72de2dd9bf638
chintan-harkhani33 <chintan.harkhani@ghanshyamdigital.com>
2024-06-07 12:48:32 +0530

    comment recording code

--- src/agoraapi/agoraapi.service.ts
+++ src/agoraapi/agoraapi.service.ts
@@ -8,6 +8,7 @@ export class AgoraapiService {
   constructor(private httpService: HttpService) {}
   private readonly logger = new Logger(AgoraapiService.name);
   async AgoraToken(channelName: string): Promise<any> {
+    try {
     let max = 4294967295; 
     let uid = Math.floor(Math.random() * (max - 1 + 1)) + 1;
     const ExpireTime = parseInt(process.env.TOKEN_EXPIRE_TIME, 10);
@@ -27,7 +28,6 @@ export class AgoraapiService {
       privilege_expire,
     );
 
-    try {
       return {
         data: {
           channelName,
@@ -45,106 +45,106 @@ export class AgoraapiService {
   }
 
  
-  private readonly AGORA_API_BASE_URL = 'https://api.agora.io/v1/apps';
-  private readonly AGORA_APP_ID = process.env.AGORA_APPID;
-  private readonly CUSTOMER_ID = process.env.CUSTOMER_ID;
-  private readonly CUSTOMER_CERTIFICATE = process.env.CUSTOMER_CERTIFICATE;
+  // private readonly AGORA_API_BASE_URL = 'https://api.agora.io/v1/apps';
+  // private readonly AGORA_APP_ID = process.env.AGORA_APPID;
+  // private readonly CUSTOMER_ID = process.env.CUSTOMER_ID;
+  // private readonly CUSTOMER_CERTIFICATE = process.env.CUSTOMER_CERTIFICATE;
 
-  private get authorizationHeader(): string {
-    return Buffer.from(
-      `${this.CUSTOMER_ID}:${this.CUSTOMER_CERTIFICATE}`,
-    ).toString('base64');
-  }
+  // private get authorizationHeader(): string {
+  //   return Buffer.from(
+  //     `${this.CUSTOMER_ID}:${this.CUSTOMER_CERTIFICATE}`,
+  //   ).toString('base64');
+  // }
   
 
-  async recordingStart(channel: string, uid: string, token: string) {
-    try {
+  // async recordingStart(channel: string, uid: string, token: string) {
+  //   try {
     
-      console.time('Checking acquireResource');
-      const resourceId = await this.acquireResource(channel, uid);
-      const startUrl = `${this.AGORA_API_BASE_URL}/${this.AGORA_APP_ID}/cloud_recording/resourceid/${resourceId}/mode/individual/start`;
-      const data = {
-        cname: channel,
-        uid: uid,
-        clientRequest: {
-          token,
-          recordingConfig: {
-            maxIdleTime: 10,
-            channelType: 1,
-            streamTypes: 2,
-            streamMode: 'default',
-            videoStreamType: 0,
-            // subscribeVideoUids: ["1"],
-          // subscribeAudioUids: ["1"],
-          subscribeUidGroup: 0,
+  //     console.time('Checking acquireResource');
+  //     const resourceId = await this.acquireResource(channel, uid);
+  //     const startUrl = `${this.AGORA_API_BASE_URL}/${this.AGORA_APP_ID}/cloud_recording/resourceid/${resourceId}/mode/individual/start`;
+  //     const data = {
+  //       cname: channel,
+  //       uid: uid,
+  //       clientRequest: {
+  //         token,
+  //         recordingConfig: {
+  //           maxIdleTime: 10,
+  //           channelType: 1,
+  //           streamTypes: 2,
+  //           streamMode: 'default',
+  //           videoStreamType: 0,
+  //           // subscribeVideoUids: ["1"],
+  //         // subscribeAudioUids: ["1"],
+  //         subscribeUidGroup: 0,
          
-            // subscribeUidGroup:1,
-          //   transcodingConfig: {
-          //     width: 360,
-          //     height: 640,
-          //     bitrate: 500,
-          //     fps: 15
-          //     mixedVideoLayout: 1,
-          // }
-          },
-          recordingFileConfig: {
-            avFileType: ["hls"],
-          },
-          storageConfig: {
-            vendor: 1,
-            region: 0,
-            bucket: process.env.AWS_BUCKET,
-            accessKey:  process.env.AWS_ACCESSKEY_ID,
-            secretKey:process.env.AWS_SECRET_KEY,
-            fileNamePrefix: ['demorecording2'],
-          },
-        },
-      };
-      const datas = await firstValueFrom(this.httpService
-        .post(startUrl, data, {
-          headers: {
-            Authorization: `Basic ${this.authorizationHeader}`,
-            'Content-Type': 'application/json',
-          },
-        }));
-      // console.log('Response', data);
-      console.timeEnd('Checking acquireResource');
-      // return startsResponse.data;
-      // console.log('data', data);
+  //           // subscribeUidGroup:1,
+  //         //   transcodingConfig: {
+  //         //     width: 360,
+  //         //     height: 640,
+  //         //     bitrate: 500,
+  //         //     fps: 15
+  //         //     mixedVideoLayout: 1,
+  //         // }
+  //         },
+  //         recordingFileConfig: {
+  //           avFileType: ["hls"],
+  //         },
+  //         storageConfig: {
+  //           vendor: 1,
+  //           region: 0,
+  //           bucket: process.env.AWS_BUCKET,
+  //           accessKey:  process.env.AWS_ACCESSKEY_ID,
+  //           secretKey:process.env.AWS_SECRET_KEY,
+  //           fileNamePrefix: ['demorecording2'],
+  //         },
+  //       },
+  //     };
+  //     const datas = await firstValueFrom(this.httpService
+  //       .post(startUrl, data, {
+  //         headers: {
+  //           Authorization: `Basic ${this.authorizationHeader}`,
+  //           'Content-Type': 'application/json',
+  //         },
+  //       }));
+  //     // console.log('Response', data);
+  //     console.timeEnd('Checking acquireResource');
+  //     // return startsResponse.data;
+  //     // console.log('data', data);
       
-      return datas.data;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  //     return datas.data;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  async acquireResource(channel: string, uid: string): Promise<any> {
-    try {
-      const acquireResponse = await firstValueFrom(this.httpService
-        .post(
-          `${this.AGORA_API_BASE_URL}/${this.AGORA_APP_ID}/cloud_recording/acquire`,
-          {
-            cname: channel,
-            uid: uid,
-            clientRequest: {
-              resourceExpiredHour: 24,
-            },
-          },
-          {
-            headers: {
-              Authorization: `Basic ${this.authorizationHeader}`,
-              'Content-Type': 'application/json',
-            },
-          },
-        ))
-      const resourceId = acquireResponse.data.resourceId;
-      console.log(resourceId);
-      this.logger.log(`Resource ID acquired: ${resourceId}`);
-      return resourceId;
-    } catch (error) {
-      console.error('Error querying recording status:', error);
-      throw error;
-    }
-  }
+  // async acquireResource(channel: string, uid: string): Promise<any> {
+  //   try {
+  //     const acquireResponse = await firstValueFrom(this.httpService
+  //       .post(
+  //         `${this.AGORA_API_BASE_URL}/${this.AGORA_APP_ID}/cloud_recording/acquire`,
+  //         {
+  //           cname: channel,
+  //           uid: uid,
+  //           clientRequest: {
+  //             resourceExpiredHour: 24,
+  //           },
+  //         },
+  //         {
+  //           headers: {
+  //             Authorization: `Basic ${this.authorizationHeader}`,
+  //             'Content-Type': 'application/json',
+  //           },
+  //         },
+  //       ))
+  //     const resourceId = acquireResponse.data.resourceId;
+  //     console.log(resourceId);
+  //     this.logger.log(`Resource ID acquired: ${resourceId}`);
+  //     return resourceId;
+  //   } catch (error) {
+  //     console.error('Error querying recording status:', error);
+  //     throw error;
+  //   }
+  // }
 
 }
